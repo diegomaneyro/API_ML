1e5f6b21094d6bc8276add464e331a9f25777810
Diego <diegomaneyro@gmail.com>
2023-03-20 02:40:11 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,85 +1,221 @@
-# Importar librerias necesarias
+# importar librerias
 from fastapi import FastAPI
-import csv
 import pandas as pd
 
-
+# Cargar el archivo CSV como un DataFrame de Pandas
+df = pd.read_csv("https://github.com/diegomaneyro/API_ML/raw/main/peliculas_final.csv")
 
 # Inicializa en el objeto app la libreria
-app = FastAPI(title='API-ML', description='Api de consulta para peliculas y series en plataformas de streming. \n By Diego Maneyro', version='1.0.1')
+app = FastAPI(title='API-ML', description='Api de consulta para peliculas y series en plataformas de streming. \n By Diego Maneyro', version='1.0.2')
+
+# Crear la columna 'platform'
+df['platform'] = df['id'].str[0]
 
 # Conexión inicial
 @app.get("/")
 async def Inicio():
-    return {"Test":"API_ML conexión realizada",
-            }
+    return {"API_ML":{"Test":"API_ML conexión realizada"}}
+import pandas as pd
 
+
+# Definir la ruta y la función correspondiente para obtener la película con mayor duración
+@app.get("/max_duration/")
+def get_max_duration(year: int = None, platform: str = None, duration_type: str = None):
+    if platform=='netflix':
+        platform='n'
+    if platform=='amazon':
+        platform='a'
+    if platform=='hulu':
+        platform='h'
+    if platform=='disney':
+        platform='d'
+     # Filtrar por año, plataforma y tipo de duración si se especifican
+    if year:
+        df_filtered = df[df['release_year'] == year]
+    else:
+        df_filtered = df
+    if platform:
+        df_filtered = df_filtered[df_filtered['platform'] == platform]
+    if duration_type:
+        df_filtered = df_filtered[df_filtered['duration_type'] == duration_type]
+
+    # Encontrar la película con la duración máxima
+    max_duration = df_filtered[df_filtered['duration_int'] == df_filtered['duration_int'].max()]
+
+    # Retornar el título y la duración de la película como un diccionario
+    return max_duration[['title', 'duration_int']].to_dict('records')
+    
+
+# Definir la ruta y la función correspondiente para obtener la cantidad de películas por plataforma con un puntaje mayor a XX en determinado año
+@app.get("/score_count/")
+def get_score_count(platform: str, scored: float, year: int):
+    if platform=='netflix':
+        platform='n'
+    if platform=='amazon':
+        platform='a'
+    if platform=='hulu':
+        platform='h'
+    if platform=='disney':
+        platform='d'
+    score_count = df.copy()
+    
+    # Obtener las películas de la plataforma y del año ingresados por parámetro
+    score_count = score_count[(score_count["id"].str[0].str.lower() == platform.lower()) & (score_count["release_year"] == year)]
+    
+    # Obtener la cantidad de películas con el puntaje mayor al ingresado por parámetro
+    score_count = score_count[score_count["rating"] >= scored]
+    count = len(score_count)
+    
+    # Retornar la cantidad de películas
+    return {"platform": platform, "scored": scored, "year": year, "count": count}
+
+# Definir la ruta y la función correspondiente para obtener la cantidad de películas por plataforma
+@app.get("/count_platform/")
+def get_count_platform(platform: str):
+    if platform=='netflix':
+        platform='n'
+    if platform=='amazon':
+        platform='a'
+    if platform=='hulu':
+        platform='h'
+    if platform=='disney':
+        platform='d'
+    count_platform = df.copy()
+    
+    # Obtener las películas de la plataforma ingresada por parámetro
+    count_platform = count_platform[count_platform["id"].str[0].str.lower() == platform.lower()]
+    
+    # Obtener la cantidad de películas
+    count = len(count_platform)
+    
+    # Retornar la cantidad de películas
+    return {"platform": platform, "count": count}
+
+# Definir la ruta y la función correspondiente para obtener el actor que más se repite según plataforma y año
+@app.get("/actor/")
+def get_actor(platform: str, year: int):
+    if platform=='netflix':
+        platform='n'
+    if platform=='amazon':
+        platform='a'
+    if platform=='hulu':
+        platform='h'
+    if platform=='disney':
+        platform='d'
+    # Filtrar por plataforma y año
+    df_filtered = df[(df['platform'] == platform) & (df['release_year'] == year)]
+
+    # Contar los actores que más se repiten
+    actor_count = df_filtered['cast'].str.split(', ', expand=True).stack().value_counts()
+
+    # Obtener el actor con el mayor recuento
+    top_actor = actor_count.index[0]
+
+    # Retornar el nombre del actor como un diccionario
+    return {'actor': top_actor}
+
+
+"""
+@app.get("/get_max_duration")
+def get_max_duration(year: int = None, platform: str = None, duration_type: str = None):
+    # Filtrar por año, plataforma y tipo de duración si se especifican
+    if year:
+        df_filtered = df[df['release_year'] == year]
+    else:
+        df_filtered = df
+    if platform:
+        df_filtered = df_filtered[df_filtered['platform'] == platform]
+    if duration_type:
+        df_filtered = df_filtered[df_filtered['duration_type'] == duration_type]
+
+    # Encontrar la película con la duración máxima
+    max_duration = df_filtered[df_filtered['duration_int'] == df_filtered['duration_int'].max()]
+
+    # Retornar el título y la duración de la película como un diccionario
+    return max_duration[['title', 'duration_int']].to_dict('records')
+
+@app.get("/get_score_count")
+def get_score_count(platform: str, scored: float, year: int = None):
+    # Filtrar por plataforma y año si se especifican
+    df_filtered = df[df['platform'] == platform]
+    if year:
+        df_filtered = df_filtered[df_filtered['release_year'] == year]
+
+    # Contar el número de películas con puntaje mayor a 'scored'
+    count = len(df_filtered[df_filtered['score'] >= scored])
+
+    # Retornar la cuenta como un diccionario
+    return {'count': count}
+
+@app.get("/get_count_platform")
+def get_count_platform(platform: str):
+    # Filtrar por plataforma y contar el número de películas
+    count = len(df[df['platform'] == platform])
+
+    # Retornar la cuenta como un diccionario
+    return {'count': count}
+
+@app.get("/get_actor")
+def get_actor(platform: str, year: int):
+    # Filtrar por plataforma y año
+    df_filtered = df[(df['platform'] == platform) & (df['release_year'] == year)]
+
+    # Contar los actores que más se repiten
+    actor_count = df_filtered['cast'].str.split(', ', expand=True).stack().value_counts()
+
+    # Obtener el actor con el mayor recuento
+    top_actor = actor_count.index[0]
+
+    # Retornar el nombre del actor como un diccionario
+    return {'actor': top_actor}
+"""
+
+
+
+
+
+
+
+
+
+'''
 # Menu
 @app.get("/menu")
 async def menu():
     return {"Consultas de API_ML": 
             {"get_max_duration" : "Pelicula con mayor duracion","get_score_count" : "Cantidad de películas por plataforma por puntaje","get_count_plataforma" : "Cantidad de películas por plataforma con filtro de plataforma", "get_actor" : "Actor que más se repite según plataforma y año"}}
 
+@app.get("/get_max_duration")
+async def get_max_duration(year: int = None, platform: str = None, duration_type: str = None):
+    df = datos.copy()
+    if year is not None:
+        df = df[df['release_year'] == year]
+    if platform is not None:
+        df = df[df['plataforma'] == platform]
+    if duration_type is not None:
+        df = df[df['duration_type'] == duration_type]
+    max_duration = df[df['duration_int'] == df['duration_int'].max()]
+    return max_duration[['title', 'duration_int']].to_dict(orient='records')
 
-# Película con mayor duración con filtros opcionales de AÑO, PLATAFORMA Y TIPO DE DURACIÓN    
-@app.get('/get_max_duration/{year}/{platform}/{duration_type}') 
-def get_max_duration(year: int = None, platform: str = None, duration_type: str = None):
-    df = pd.read_csv('peliculas_final.csv')
-    # Aplicar los filtros opcionales al DataFrame
-    if year:
-        df_filtered = df[df['release_year'] == year]
-    else:
-        df_filtered = df.copy()
-    if platform:
-        df_filtered = df_filtered[df_filtered['plataforma'] == platform]
-    if duration_type:
-        df_filtered = df_filtered[df_filtered['duration_type'] == duration_type]
-    # Encontrar la película con mayor duración en el DataFrame filtrado
-        max_duration_movie = df_filtered.loc[df_filtered['duration_int'].idxmax()]
-    # Retornar la película con mayor duración como un diccionario
-    return max_duration_movie.to_dict()  
+@app.get("/get_score_count")
+async def get_score_count(platform: str, scored: float, year: int):
+    df = datos.copy()
+    df = df[(df['plataforma'] == platform) & (df['score'] >= scored) & (df['release_year'] == year)]
+    count = df['plataforma'].value_counts().to_dict()
+    return count
 
+@app.get("/get_count_platform")
+async def get_count_platform(platform: str):
+    df = datos.copy()
+    df = df[df['plataforma'] == platform]
+    count = df['plataforma'].value_counts().to_dict()
+    return count
 
-# Cantidad de películas por plataforma con un puntaje mayor a XX en determinado año
-@app.get("/get_score_count/{platform}/{scored}/{year}")
-def get_score_count(platform: str, scored: int, year: int):
-    df = pd.read_csv('peliculas_final.csv')
-    # Filtrar las películas por plataforma, puntaje y año
-    df_filtered = df[(df['plataforma'] == platform) & (df['score'] > scored) & (df['release_year'] == year)]
-    # Contar el número de películas en el DataFrame filtrado
-    count = df_filtered['title'].count()
-    # Retornar la cantidad de películas como un diccionario
-    return {'count': count}
+@app.get("/get_actor")
+async def get_actor(platform: str, year: int):
+    df = datos.copy()
+    df = df[(df['plataforma'] == platform) & (df['release_year'] == year)]
+    actor = df['cast'].value_counts().index[0]
+    return actor
 
-# Cantidad de películas por plataforma con filtro de plataforma
-@app.get("/get_count_plataforma/{platform}")
-def get_count_platform(platform: str):
-    df = pd.read_csv('peliculas_final.csv')
-    # Filtrar las películas por plataforma
-    df_filtered = df[df['plataforma'] == platform]
-    # Contar el número de películas en el DataFrame filtrado
-    count = df_filtered['title'].count()
-    # Retornar la cantidad de películas como un diccionario
-    return {'count': count}
-
-
-# Actor que más se repite según plataforma y año. (La función debe llamarse get_actor(platform, year))
-@app.get("/get_actor/{platform}/{year}")
-def get_actor(platform: str, year: int):
-    df = pd.read_csv('peliculas_final.csv')
-    # Filtrar las películas por plataforma y año
-    df_filtered = df[(df['plataforma'] == platform) & (df['release_year'] == year)]
-    # Contar la cantidad de veces que aparece cada actor en el DataFrame filtrado
-    actor_counts = df_filtered['cast'].str.split(', ', expand=True).stack().value_counts()
-    # Encontrar el actor que más se repite
-    actor_most_frequent = actor_counts.index[0]
-    # Retornar el actor que más se repite como un diccionario
-    return {'actor': actor_most_frequent}
-
-
-# Datos de contacto 
-@app.get("/Autor")
-async def Datos_Contacto():
-    contacto = {"Nombre":"Diego Maneyro", "email":"diegomaneyro@gmail.com","github":"https://github.com/diegomaneyro"}
-    return contacto
-
+'''

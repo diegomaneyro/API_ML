84609873549c3e7e8773eefcabe4f00e18238d01
diego <diegomaneyro@gmail.com>
2023-06-05 11:52:44 -0300

    mdifique main:api

--- main.py
+++ main.py
@@ -1,53 +1,80 @@
-# importar librerias
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import pandas as pd
+from pandasql import sqldf
 
-# Cargar el archivo CSV como un DataFrame de Pandas
-df = pd.read_csv("./datos/csvs/peliculas/peliculas_final.csv")
-
+# ruta de datos de peliculas
+url = "datos/csvs/peliculas/peliculas_final.csv"
+datos = pd.read_csv(url, sep=',', encoding='latin-1')
 # Inicializa en el objeto app la libreria
 app = FastAPI(title='API-ML', description='Api de consulta para peliculas y series en plataformas de streming. \n By Diego Maneyro', version='1.0.2')
-
-# Crear la columna 'platform'
-df['platform'] = df['id'].str[0]
 
 # Conexión inicial
 @app.get("/")
 async def Inicio():
-    return {"API_ML":{"Test":"API_ML conexión realizada"}}
+    return {"Inicio":"Api_ml"}
+@app.get("/verificar_conexion")
+def verificar_conexion():
+    try:
+        # Cargar los datos del archivo CSV en un DataFrame de Pandas
+        #url = "https://drive.google.com/file/d/16cYwPRDnOyoKe8fzX2vSnaJFtmQMdTwq/view?usp=drive_link"
+        url = "datos/csvs/peliculas/peliculas_final.csv"
+        datos = pd.read_csv(url, sep=',', encoding='latin-1')
+        return {"Message": "Conexion Exitosa"}
+    except FileNotFoundError:
+        return {"Message": "No se encontró el archivo de datos."}
+    except Exception as e:
+        return {"Message": f"Error en la conexión a los datos: {str(e)}"}
 
-
-# Definir la ruta y la función correspondiente para obtener la película con mayor duración
-@app.get("/max_duration/")
+# Definir la ruta para la consulta
+@app.get("/max_duration")
 def get_max_duration(year: int = None, platform: str = None, duration_type: str = None):
-    if platform=='netflix':
-        platform='n'
-    if platform=='amazon':
-        platform='a'
-    if platform=='hulu':
-        platform='h'
-    if platform=='disney':
-        platform='d'
-     # Filtrar por año, plataforma y tipo de duración si se especifican
-    if year:
-        df_filtered = df[df['release_year'] == year]
-    else:
-        df_filtered = df
-    if platform:
-        df_filtered = df_filtered[df_filtered['platform'] == platform]
-    if duration_type:
-        df_filtered = df_filtered[df_filtered['duration_type'] == duration_type]
-
-    # Encontrar la película con la duración máxima
-    max_duration = df_filtered[df_filtered['duration_int'] == df_filtered['duration_int'].max()]
-
-    # Retornar el título y la duración de la película como un diccionario
-    return max_duration[['title', 'duration_int']].to_dict('records')
+    # Cargar los datos del archivo CSV en un DataFrame de Pandas
+    datos = pd.read_csv(url, sep=',', encoding='latin-1')
+        
+    # Crear la consulta SQL base
+    query = """
+        SELECT *
+        FROM datos
+        WHERE 1 = 1
+    """    
+    # Agregar condiciones opcionales según los parámetros proporcionados
+    if year is not None:
+        query += f" AND release_year = {year}"
+    if platform is not None:
+        # Convertir la plataforma a minúsculas
+        query = query = f"""
+                SELECT *
+                FROM datos
+                WHERE LOWER(SUBSTR(id, 1, 1)) = '{platform[0]}'
+            """
+        df_filtrado = sqldf(query, locals())            
+    if duration_type is not None:
+        query += f" AND duration_type = '{duration_type}'"
     
+    # Agregar ordenamiento y limitación
+    query += " ORDER BY duration_int DESC LIMIT 1"
+    
+    # Ejecutar la consulta utilizando pandasql
+    df_filtrado = sqldf(query, locals())
+    
+    # Verificar si se encontraron resultados
+    if df_filtrado.empty:
+        raise HTTPException(status_code=404, detail="No se encontraron resultados.")
+    
+    # Obtener el título del DataFrame filtrado
+    titulo = df_filtrado["title"].values[0]
+    
+    # Crear el diccionario de respuesta
+    respuesta = {'title': titulo}
+    
+    # Devolver la respuesta
+    return respuesta
+
 
 # Definir la ruta y la función correspondiente para obtener la cantidad de películas por plataforma con un puntaje mayor a XX en determinado año
 @app.get("/score_count/")
 def get_score_count(platform: str, scored: str, year: int):
+    datos = pd.read_csv(url)
     if platform=='netflix':
         platform='n'
     if platform=='amazon':
@@ -56,7 +83,7 @@ def get_score_count(platform: str, scored: str, year: int):
         platform='h'
     if platform=='disney':
         platform='d'
-    score_count = df.copy()
+    score_count = datos.copy()
     
     # Obtener las películas de la plataforma y del año ingresados por parámetro
     score_count = score_count[(score_count["id"].str[0].str.lower() == platform.lower()) & (score_count["release_year"] == year)]
@@ -79,7 +106,7 @@ def get_count_platform(platform: str):
         platform='h'
     if platform=='disney':
         platform='d'
-    count_platform = df.copy()
+    count_platform = datos.copy()
     
     # Obtener las películas de la plataforma ingresada por parámetro
     count_platform = count_platform[count_platform["id"].str[0].str.lower() == platform.lower()]
@@ -101,15 +128,12 @@ def get_actor(platform: str, year: int):
         platform='h'
     if platform=='disney':
         platform='d'
-    # Filtrar por plataforma y año
-    df_filtered = df[(df['platform'] == platform) & (df['release_year'] == year)]
-
+    
     # Contar los actores que más se repiten
-    actor_count = df_filtered['cast'].str.split(', ', expand=True).stack().value_counts()
+    actor_count = datos['cast'].str.split(', ', expand=True).stack().value_counts()
 
     # Obtener el actor con el mayor recuento
     top_actor = actor_count.index[0]
 
     # Retornar el nombre del actor como un diccionario
     return {'actor': top_actor}
-

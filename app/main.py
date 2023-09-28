d910974159904009ac812d1f861e761e056ff4be
diego <diegomaneyro@gmail.com>
2023-09-28 13:28:13 -0300

    modifico archivo main.py.agrego archivo dockerfile.restructura de app

--- app/main.py
+++ app/main.py
@@ -3,36 +3,37 @@ import pandas as pd
 from pandasql import sqldf
 
 # Inicializa en el objeto app la libreria
-app = FastAPI(title='ApiStream', description='Api de consulta para peliculas y series en plataformas de streming. \n By Diego Maneyro', version='1.0.3')
+app = FastAPI(title='ApiStream', description='Api de consulta para peliculas y series en plataformas de streming. \n By Diego Maneyro', version='1.0.4')
 
-# Conexión inicial
+# Leer datos desde un contenedor en cuenta de almacenamiento en Microsoft Azure
+url = "https://apistreamdatos.blob.core.windows.net/apidatos/peliculas_final.csv"
+
+# inicio
 @app.get("/")
 async def Inicio():
     return {"Inicio":"Api_ml"}
+
 @app.get("/autor")
 async def autor():
     datos = {"Nombre y Apellido":"Diego Maneyro",
             "Email":"diegomaneyro@gmail.com"}
     return datos
+
 @app.get("/verificar_conexion")
-def verificar_conexion():
+async def verificar_conexion():
     try:
-        # Cargar los datos del archivo CSV en un DataFrame de Pandas
-        url = "datos/csvs/peliculas/peliculas_final.csv"
+        # Leer los datos del archivo CSV
         datos = pd.read_csv(url, sep=',', encoding='latin-1')
-        return {"Message": "Conexion Exitosa"}
-    except FileNotFoundError:
-        return {"Message": "No se encontró el archivo de datos."}
+        return {"Message": "Conexión Exitosa"}
     except Exception as e:
         return {"Message": f"Error en la conexión a los datos: {str(e)}"}
 
+
 # Definir la ruta para la consulta
 @app.get("/max_duration")
-def get_max_duration(year: int = None, platform: str = None, duration_type: str = None):
-    # Cargar los datos del archivo CSV en un DataFrame de Pandas
-    url = "datos/csvs/peliculas/peliculas_final.csv"     
-    datos = pd.read_csv(url, sep=',', encoding='latin-1')
-        
+async def get_max_duration(year: int = None, platform: str = None, duration_type: str = None):
+    # Cargar los datos del archivo CSV en un DataFrame de Pandas           
+    datos = pd.read_csv(url, sep=',', encoding='latin-1')        
     # Crear la consulta SQL base
     query = """
         SELECT *
@@ -75,8 +76,7 @@ def get_max_duration(year: int = None, platform: str = None, duration_type: str 
 
 # Definir la ruta y la función correspondiente para obtener la cantidad de películas por plataforma con un puntaje mayor a XX en determinado año
 @app.get("/score_count/")
-def get_score_count(platform: str, scored: str, year: int):
-    url = "datos/csvs/peliculas/peliculas_final.csv"     
+async def get_score_count(platform: str, scored: str, year: int):    
     datos = pd.read_csv(url)
     if platform=='netflix':
         platform='n'
@@ -100,8 +100,7 @@ def get_score_count(platform: str, scored: str, year: int):
 
 # Definir la ruta y la función correspondiente para obtener la cantidad de películas por plataforma
 @app.get("/count_platform/")
-def get_count_platform(platform: str):
-    url = "datos/csvs/peliculas/peliculas_final.csv"
+async def get_count_platform(platform: str):
     datos = pd.read_csv(url, sep=',', encoding='latin-1')     
     if platform=='netflix':
         platform='n'
@@ -131,10 +130,8 @@ def get_count_platform(platform: str):
 
 # Definir la ruta y la función correspondiente para obtener el actor que más se repite según plataforma y año
 @app.get("/actor/")
-def get_actor(platform: str, year: int):       
-    url = "datos/csvs/peliculas/peliculas_final.csv"
+async def get_actor(platform: str, year: int):       
     datos = pd.read_csv(url, sep=',', encoding='latin-1')  
-
     if platform=='netflix':
         platform='n'
     if platform=='amazon':
